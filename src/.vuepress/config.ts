2babb764bbeeffb07d2449703e04dab7021ceddb
lwx1095775 <lzc525@qq.com>
2024-01-10 23:55:33 +0800

    调试base路径样式

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/lzc-blog.github.io/",
 
   lang: "zh-CN",
   title: "lzc的博客",
